9b2aaebcc1f5683bcccb4302df16362996dae592
gandhipratik65 <gandhipratik65@gmail.com>
2023-03-13 20:16:40 -0700

    basic train data

---
+++ python/useractivity.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+# load the csv file into a pandas dataframe
+df = pd.read_csv("train.csv")
+
+# print the first few rows of the dataframe
+print(df.head())
+
+# Calculate descriptive statistics
+print(df.describe())
+
+# Create a histogram of the tBodyAcc-mean()-X column
+plt.hist(df['tBodyAcc-mean()-X'], bins=50)
+plt.title('tBodyAcc-mean()-X')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+plt.show()
+
+# Create scatter plot for tBodyAcc-mean()-X and tBodyAcc-mean()-Y
+plt.scatter(df['tBodyAcc-mean()-X'], df['tBodyAcc-mean()-Y'])
+
+# Set axis labels and plot title
+plt.xlabel('tBodyAcc-mean()-X')
+plt.ylabel('tBodyAcc-mean()-Y')
+plt.title('Scatter plot for tBodyAcc-mean()-X and tBodyAcc-mean()-Y')
+
+# Show plot
+plt.show()
+
+
+# Create a box plot for each feature
+#for feature in df.columns[:-1]:
+ #   sns.boxplot(x='Activity', y=feature, data=df)
+    
+    
+# Preprocess data
+X = df.values
+
+# Choose number of clusters
+k = 3
+
+# Apply k-means algorithm
+kmeans = KMeans(n_clusters=k, random_state=0).fit(X[:, :-1])
+
+# Get cluster labels
+labels = kmeans.labels_
+
+# Get cluster centers
+centers = kmeans.cluster_centers_
+
+# Visualize results
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='viridis')
+plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+plt.show()
